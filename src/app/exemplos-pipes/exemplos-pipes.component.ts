927f09d25e6f8ad383e0f21587c18d648cb656f4
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-16 10:23:01 -0300

    'pipes'

---
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'Beginning Angular with Typescript (updated to Angular)',
+    rating: 4.54321,
+    numeroPaginas: 205,
+    preco: 86.71,
+    dataLancamento: new Date(2020, 4 , 6),
+    url:"https://ler.amazon.com.br/kp/embed?asin=B01N9ZUHBA&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_XbG2Fb29QG43B"
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
